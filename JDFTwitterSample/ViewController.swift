cf3b7b95425cfde581cea5e3218aac7ade3109c3
Joe Fryer <joe.d.fryer@gmail.com>
2014-07-12 22:49:49 +0100

    Initial Commit

---
+++ JDFTwitterSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JDFTwitterSample
+//
+//  Created by Joe Fryer on 12/07/2014.
+//  Copyright (c) 2014 Joe Fryer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
