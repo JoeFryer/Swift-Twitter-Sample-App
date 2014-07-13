9821cf143300a52eae9a7d87ca1b87f5713c32e0
Joe Fryer <joe.d.fryer@gmail.com>
2014-07-13 18:47:48 +0100

    Super basic start - authenticating using the system accounts, and showing some tweets.

--- JDFTwitterSample/Classes/ViewControllers/LoginViewController.swift
+++ JDFTwitterSample/Classes/ViewControllers/LoginViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import Accounts
+import Social
 
 let LoginViewControllerTwitterButtonEdgeLength: CGFloat = 100.0
 
 
 class LoginViewController: UIViewController {
+    
+    var twitterButton: UIButton?
+    let useACAccount = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +25,76 @@ class LoginViewController: UIViewController {
     }
     
     func setupView() {
-        let twitterButton = UIButton(frame: CGRectMake((self.view.frame.size.width / 2) - (LoginViewControllerTwitterButtonEdgeLength / 2), (self.view.frame.size.height / 2) - LoginViewControllerTwitterButtonEdgeLength, LoginViewControllerTwitterButtonEdgeLength, LoginViewControllerTwitterButtonEdgeLength))
-        twitterButton.setImage(UIImage(named: JDFTwitterSampleImageNameTwitterButton), forState: UIControlState.Normal)
-        twitterButton.addTarget(self, action:"twitterButtonPressed", forControlEvents: UIControlEvents.TouchUpInside);
-        self.view.addSubview(twitterButton)
+        self.twitterButton = UIButton(frame: CGRectMake((self.view.frame.size.width / 2) - (LoginViewControllerTwitterButtonEdgeLength / 2), (self.view.frame.size.height / 2) - LoginViewControllerTwitterButtonEdgeLength, LoginViewControllerTwitterButtonEdgeLength, LoginViewControllerTwitterButtonEdgeLength))
+        self.twitterButton!.setImage(UIImage(named: JDFTwitterSampleImageNameTwitterButton), forState: UIControlState.Normal)
+        self.twitterButton!.addTarget(self, action:"twitterButtonPressed", forControlEvents: UIControlEvents.TouchUpInside);
+        self.view.addSubview(self.twitterButton)
     }
     
     func twitterButtonPressed() {
-        println("Twitter button pressed")
+        let failureHandler: ((NSError) -> Void) = {
+            error in
+            
+            self.alert(error.localizedDescription)
+        }
+        
+        if useACAccount {
+            let accountStore = ACAccountStore()
+            let accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+            
+            accountStore.requestAccessToAccountsWithType(accountType, options: nil) {
+                granted, error in
+                
+                if granted {
+                    let twitterAccounts = accountStore.accountsWithAccountType(accountType)
+                    
+                    if twitterAccounts {
+                        if twitterAccounts.count == 0 {
+                            self.alert("There are no Twitter accounts configured. You can add or create a Twitter account in Settings.")
+                        } else {
+                            let twitterAccount = twitterAccounts[0] as ACAccount
+                            let swifter = Swifter(account: twitterAccount)
+                            self.presentTimelineVCWithSwifter(swifter)
+                        }
+                    } else {
+                        self.alert("There are no Twitter accounts configured. You can add or create a Twitter account in Settings.")
+                    }
+                }
+            }
+        } else {
+            let swifter = Swifter(
+                consumerKey: "Cw43V1kgpeCWiJPUKARXCe3pF",
+                consumerSecret: "9AqmJ1kdq5t3W9LONUlIBiRJqiA4QqFl25fcNxGoxHUbaXjNZ7"
+            )
+            
+            swifter.authorizeWithCallbackURL(
+                NSURL(string: "swifter://success"),
+                success: {
+                    accessToken, response in
+                    
+                    self.alert("Successfully authorized with App API")
+                    self.presentTimelineVCWithSwifter(swifter)
+                },
+                failure: failureHandler
+            )
+        }
+    }
+    
+    func alert(message: String) {
+        // This is the iOS8 way of doing alerts. For older versions, look into UIAlertView
+        var alert = UIAlertController(
+            title: nil,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func presentTimelineVCWithSwifter(swifter: Swifter) {
+        let timeLineVC = TimelineViewController()
+        let navigationController = UINavigationController(rootViewController: timeLineVC)
+        timeLineVC.account = swifter
+        self.presentViewController(navigationController, animated: true, completion: nil)
     }
 }
-
-
-
---
+++ JDFTwitterSample/Classes/ViewControllers/TimelineViewController.swift
@@ -0,0 +1,94 @@
+//
+//  TimelineViewController.swift
+//  JDFTwitterSample
+//
+//  Created by Joe Fryer on 13/07/2014.
+//  Copyright (c) 2014 Joe Fryer. All rights reserved.
+//
+
+import UIKit
+
+let TimelineViewControllerCellIdentifier = "TimelineViewControllerCellIdentifier"
+
+class TimelineViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // Data
+    var account: Swifter?
+    var tweets: [JSONValue] = []
+    
+    // Views
+    var tableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Tweets"
+        
+
+        self.setupView()
+        self.tableView!.registerClass(UITableViewCell.self, forCellReuseIdentifier: TimelineViewControllerCellIdentifier)
+        self.fetchTwitterHomeStream()
+    }
+    
+    func setupView() {
+        self.tableView = UITableView(frame: self.view.bounds, style: UITableViewStyle.Plain)
+        self.view.addSubview(self.tableView)
+        self.tableView!.dataSource = self
+        self.tableView!.delegate = self
+    }
+    
+    func fetchTwitterHomeStream() {
+        let failureHandler: ((NSError) -> Void) = {
+            error in
+            self.alert(error.localizedDescription)
+        }
+        
+        self.account!.getStatusesHomeTimelineWithCount(
+            20,
+            sinceID: nil,
+            maxID: nil,
+            trimUser: true,
+            contributorDetails: false,
+            includeEntities: true,
+            success: {
+                (statuses: [JSONValue]?) in
+                
+                if statuses {
+                    self.tweets = statuses!
+                    self.tableView?.reloadData()
+                }
+            },
+            failure: failureHandler
+        )
+    }
+    
+    func alert(message: String) {
+        var alert = UIAlertController(
+            title: nil,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        let numberOfTweets = self.tweets.count
+        println("\(numberOfTweets)")
+        return numberOfTweets
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(TimelineViewControllerCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = tweets[indexPath.row]["text"].string
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, heightForRowAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+        return 85.0
+    }
+}
